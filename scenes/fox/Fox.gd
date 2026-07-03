553fbd87e6de7a13d9b7e7c6d3da10fb96e588cd
DogWizardStudios <DogWizardStudios@gmail.com>
2026-07-03 21:40:41 +0100

    Add eating mechanic

--- scenes/fox/Fox.gd
+++ scenes/fox/Fox.gd
@@ -2,14 +2,14 @@ class_name Fox
 
 extends Area2D
 
-@export var speed: float = 200
+@export var speed: float = 400.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var munch_sound: AudioStreamPlayer2D = $MunchSound
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var move: float = Input.get_axis("fox_left", "fox_right")
-	print(Input.get_axis("fox_left", "fox_right"))
 	
 	#get_axis above outputs -1 for left and 1 for right, 0 for no input
 	#as we move approx zero is false, so true because of ! so the sprite will flip 
@@ -21,3 +21,10 @@ func _physics_process(delta: float) -> void:
 		sprite_2d.flip_h = move > 0.0
 	
 	position.x += move * speed * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("CHOMP")
+	if area is Dice:
+		area.queue_free()
+		munch_sound.play()
